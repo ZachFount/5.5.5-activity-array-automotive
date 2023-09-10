72807d45d5ead00986c063cc3e239c1f7c363390
Zach Fountain <141088304+ZachFount@users.noreply.github.com>
2023-09-10 11:49:38 -0400

    Added class and const

--- factory.js
+++ factory.js
@@ -1,3 +1,14 @@
 class Factory{
-    
-}
+    constructor(location, owner, listVehicles){
+    this.loction = location;
+    this.owner = owner;
+    this.listVehicles = listVehicles;
+    }
+    createVehicle(vehicle){
+
+    }
+
+    paintVehicle(vin, paintColor){
+        
+    }
+}    
--- vehicle.js
+++ vehicle.js
@@ -1,4 +1,18 @@
 class Vehicle {
-    
+    constructor(make, model, color, price, vin) {
+    this.make = make;
+    this.model = model;
+    this.color = color;
+    this .price = price;
+    this.vin  =  vin;
+    }
+
+    drive(){
+        return this.model + " " + "drives"
+    }
+
+    Paint(color){
+        return this.model = " " + "is GREEN"
+    }
 }
 
